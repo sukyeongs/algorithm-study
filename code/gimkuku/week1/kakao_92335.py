4e391d161fced5817d876d0703fd96d4dd099d12
gimkuku <khjyj125@gmail.com>
2022-03-07 14:27:10 +0900

    [KAKAO]92335

--- code/gimkuku/week1/kakao_92335.py
+++ code/gimkuku/week1/kakao_92335.py
@@ -1,1 +1,40 @@
+import math
+
+# 소수 판별 함수
+def is_prime_num(n):
+    # 1일때 예외 처리
+    if (n == 1) :
+        return False
     
+    sqrt_n = math.sqrt(n)
+    sqrt_n = int(sqrt_n) 
+    for i in range(2,sqrt_n +1):
+        if n % i == 0:
+            return False
+    return True
+
+# n 진법으로 바꾸는 함수 
+def dec_to_n(k,n):
+    rev_base = ''
+    while k > 0:
+        k, mod = divmod(k, n)
+        rev_base += str(mod)
+    return rev_base[::-1]
+
+# main 함수
+def solution(n, k):
+    # 숫자를 k진수로 바꿈
+    str_num = dec_to_n(n,k)
+    
+    # 0으로 토크나이징
+    list_num = str_num.split('0')
+    
+    # 리스트를 int로 바꾼 뒤 소수 판별
+    answer = 0
+    for i in list_num:
+        if i == '':
+            continue
+        if(is_prime_num(int(i))):
+            answer = answer+1 
+    
+    return answer
