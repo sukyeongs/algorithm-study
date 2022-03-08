b6ca53df133ada05618c7976b1df1384ac22a156
Sukyeong Seo <ssk0967@naver.com>
2022-03-09 01:34:53 +0900

    Update 3.py

--- code/sukyeongs/week1/3.py
+++ code/sukyeongs/week1/3.py
@@ -1,1 +1,42 @@
+import math
 
+def time_to_minutes(time):
+    h, m = map(int, time.split(':'))
+    return h*60 + m
+
+def solution(fees, records):
+    answer = []
+
+    dt, df, ut, uf = fees
+
+    dic = dict()
+
+    for r in records:
+        time, car, inout = r.split()
+        car = int(car)
+
+        if car in dic:
+            dic[car].append([time_to_minutes(time), inout])
+        else:
+            dic[car] = [[time_to_minutes(time), inout]]
+
+    rList = sorted(dic.items())
+
+    for r in rList:
+        t = 0
+
+        for rr in r[1]: 
+            if rr[1] == "IN": 
+                t -= rr[0] 
+            else: 
+                t += rr[0] 
+
+        if r[1][-1][1] == "IN": 
+            t += time_to_minutes('23:59') 
+
+        if t <= dt:
+            answer.append(df)
+        else:
+            answer.append(df + math.ceil((t-dt) / ut) * uf)
+
+    return answer
