9efd5ebac6cb946dd1e6391b78640d68b258d6be
Sukyeong Seo <ssk0967@naver.com>
2022-03-17 17:29:35 +0900

    Update 3.py

--- code/sukyeongs/week2/3.py
+++ code/sukyeongs/week2/3.py
@@ -1,1 +1,20 @@
-
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    
+    result = set(report)
+    result = list(result)
+    
+    dict = {}
+    for id in id_list:
+        dict[id] = []
+        
+    for i in result:
+        show = i.split(" ")
+        dict[show[1]] += [show[0]]
+        
+    for key, value in dict.items():
+        if len(value) >= k:
+            for person in value:
+                answer[id_list.index(person)] += 1
+                
+    return answer
