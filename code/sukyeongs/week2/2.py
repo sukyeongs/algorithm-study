13f143dd06e94db4d4670855482e321349c5bcee
Sukyeong Seo <ssk0967@naver.com>
2022-03-17 17:25:49 +0900

    Update 2.py

--- code/sukyeongs/week2/2.py
+++ code/sukyeongs/week2/2.py
@@ -1,1 +1,12 @@
-
+def solution(board, skill):
+    answer = 0
+    
+    for type, r1, c1, r2, c2, degree in skill:
+        for y in range(r1, r2+1, 1):
+            for x in range(c1, c2+1, 1):
+                board[y][x] += degree if type == 2 else -degree 
+    
+    for row in board:
+        answer += len(list(filter(lambda x: x > 0, row)))
+    
+    return answer
