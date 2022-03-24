77b666095e64e78d7ca3bf99940096660002bdb0
Sukyeong Seo <ssk0967@naver.com>
2022-03-24 12:46:44 +0900

    Update 3.py

--- code/sukyeongs/week3/3.py
+++ code/sukyeongs/week3/3.py
@@ -1,1 +1,24 @@
+def solution(enroll, referral, seller, amount):
+    answer = []
+    profit_dicts = {}
+    tree_dicts = {}
+    for i in range(len(enroll)):
+        tree_dicts[enroll[i]] = referral[i]
+        profit_dicts[enroll[i]] = 0
 
+    for i in range(len(seller)) :
+        cost = amount[i]*100
+        node = seller[i]
+        while node != '-':
+            parent = tree_dicts[node]
+            if cost*0.1 < 1:
+                profit_dicts[node] += cost
+                break
+            profit_dicts[node] += (cost-(cost//10))
+            cost //= 10
+            node = parent
+
+
+    for name in enroll :
+        answer.append(int(profit_dicts[name]))
+    return answer
